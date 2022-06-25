86dbddc81a91cb48a58918ade05af5a3d15ba141
Ilyukina Anna <ilyukinaanna@MacBook-Air-Ilyukina.local>
2022-06-25 17:22:09 +0800

    creation of the initial structure of the project

---
+++ frontend/src/App.vue
@@ -0,0 +1,33 @@
+<script>
+import FormAddProduct from './components/FormAddProduct.vue';
+import ProductList from './components/ProductList.vue';
+
+export default {
+  name: 'App',
+  components: {
+    FormAddProduct,
+    ProductList
+  }
+}
+
+
+</script>
+
+<template>
+  <section class="catalog">
+    <div class="catalog__header">
+      <h1 class="catalog__header-title">
+        Добавление товара
+      </h1>
+      <select class="catalog__header-select"/>
+    </div>
+    <div class="catalog__content">
+      <FormAddProduct/>
+      <ProductList/>
+    </div>
+  </section>
+</template>
+
+<style>
+
+</style>
